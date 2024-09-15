7f1834291f5159d9dd0343217d7937f361684035
wwIrvingww <mor22781@uvg.edu.gt>
2024-09-15 16:19:32 -0600

    some cubes added

--- src/cube.rs
+++ src/cube.rs
@@ -1,74 +1,82 @@
 use nalgebra_glm::Vec3;
 use crate::ray_intersect::{RayIntersect, Intersect};
+use std::sync::Arc;
 use crate::material::Material;
 
 pub struct Cube {
-    pub min: Vec3,
-    pub max: Vec3,
-    pub center: Vec3,  // Agregar el centro
-    pub material: Material,
+    pub center: Vec3,
+    pub size: f32,
+    pub material: Arc<Material>,  // Cambia esto para aceptar Arc<Material>
 }
 
 impl Cube {
-    // Constructor para crear un nuevo cubo usando el centro y el tamaño
-    pub fn new(center: Vec3, size: f32, material: Material) -> Self {
-        let half_size = size / 2.0;
-        let min = center - Vec3::new(half_size, half_size, half_size);
-        let max = center + Vec3::new(half_size, half_size, half_size);
-        
+    pub fn new(center: Vec3, size: f32, material: Arc<Material>) -> Self {
         Cube {
-            min,
-            max,
             center,
+            size,
             material,
         }
     }
 
-    // Función para calcular las coordenadas UV
+    // Calcula el punto mínimo del cubo (esquina inferior izquierda)
+    pub fn min(&self) -> Vec3 {
+        self.center - Vec3::new(self.size / 2.0, self.size / 2.0, self.size / 2.0)
+    }
+
+    // Calcula el punto máximo del cubo (esquina superior derecha)
+    pub fn max(&self) -> Vec3 {
+        self.center + Vec3::new(self.size / 2.0, self.size / 2.0, self.size / 2.0)
+    }
+}
+
+// Función para calcular las coordenadas UV
+impl Cube {
     pub fn get_uv(&self, point: &Vec3, normal: &Vec3) -> (f32, f32) {
         let mut u = 0.0;
         let mut v = 0.0;
-    
+
+        let min = self.min();
+        let max = self.max();
+
         // Front face
         if normal.z.abs() > 0.99 {
-            u = (point.x - self.min.x) / (self.max.x - self.min.x);
-            v = (point.y - self.min.y) / (self.max.y - self.min.y);
+            u = (point.x - min.x) / (max.x - min.x);
+            v = (point.y - min.y) / (max.y - min.y);
         }
         // Back face
         else if normal.z.abs() > 0.01 && normal.z < 0.0 {
-            u = (self.max.x - point.x) / (self.max.x - self.min.x);
-            v = (point.y - self.min.y) / (self.max.y - self.min.y);
+            u = (max.x - point.x) / (max.x - min.x);
+            v = (point.y - min.y) / (max.y - min.y);
         }
         // Left face
         else if normal.x.abs() > 0.99 && normal.x < 0.0 {
-            u = (self.max.z - point.z) / (self.max.z - self.min.z);
-            v = (point.y - self.min.y) / (self.max.y - self.min.y);
+            u = (max.z - point.z) / (max.z - min.z);
+            v = (point.y - min.y) / (max.y - min.y);
         }
         // Right face
         else if normal.x.abs() > 0.99 && normal.x > 0.0 {
-            u = (point.z - self.min.z) / (self.max.z - self.min.z);
-            v = (point.y - self.min.y) / (self.max.y - self.min.y);
+            u = (point.z - min.z) / (max.z - min.z);
+            v = (point.y - min.y) / (max.y - min.y);
         }
         // Top face
         else if normal.y.abs() > 0.99 && normal.y > 0.0 {
-            u = (point.x - self.min.x) / (self.max.x - self.min.x);
-            v = (self.max.z - point.z) / (self.max.z - self.min.z);
+            u = (point.x - min.x) / (max.x - min.x);
+            v = (max.z - point.z) / (max.z - min.z);
         }
         // Bottom face
         else if normal.y.abs() > 0.99 && normal.y < 0.0 {
-            u = (point.x - self.min.x) / (self.max.x - self.min.x);
-            v = (point.z - self.min.z) / (self.max.z - self.min.z);
+            u = (point.x - min.x) / (max.x - min.x);
+            v = (point.z - min.z) / (max.z - min.z);
         }
-    
+
         (u, v)
     }
-    
 }
 
 impl RayIntersect for Cube {
     fn ray_intersect(&self, ray_origin: &Vec3, ray_direction: &Vec3) -> Intersect {
-        let t_min = (self.min - ray_origin).component_div(ray_direction);
-        let t_max = (self.max - ray_origin).component_div(ray_direction);
+        let t_min = (self.min() - ray_origin).component_div(ray_direction);
+        let t_max = (self.max() - ray_origin).component_div(ray_direction);
 
         let t1 = t_min.zip_map(&t_max, |a, b| a.min(b));
         let t2 = t_min.zip_map(&t_max, |a, b| a.max(b));
@@ -81,26 +89,25 @@ impl RayIntersect for Cube {
             let point = ray_origin + ray_direction * distance;
 
             // Determinar la normal utilizando el centro
-            // Determinar la normal utilizando el centro
             let mut normal = Vec3::new(0.0, 0.0, 0.0);
 
             let epsilon = 0.001; // Pequeña tolerancia para comparar valores cercanos
             let center_to_point = point - self.center;
 
-            if (center_to_point.x.abs() - (self.max.x - self.center.x)).abs() < epsilon {
+            if (center_to_point.x.abs() - (self.max().x - self.center.x)).abs() < epsilon {
                 normal.x = center_to_point.x.signum(); // Normal en el eje X
-            } else if (center_to_point.x.abs() - (self.center.x - self.min.x)).abs() < epsilon {
+            } else if (center_to_point.x.abs() - (self.center.x - self.min().x)).abs() < epsilon {
                 normal.x = -center_to_point.x.signum(); // Normal en el eje X (cara opuesta)
-            } else if (center_to_point.y.abs() - (self.max.y - self.center.y)).abs() < epsilon {
+            } else if (center_to_point.y.abs() - (self.max().y - self.center.y)).abs() < epsilon {
                 normal.y = center_to_point.y.signum(); // Normal en el eje Y
-            } else if (center_to_point.y.abs() - (self.center.y - self.min.y)).abs() < epsilon {
+            } else if (center_to_point.y.abs() - (self.center.y - self.min().y)).abs() < epsilon {
                 normal.y = -center_to_point.y.signum(); // Normal en el eje Y (cara opuesta)
-            } else if (center_to_point.z.abs() - (self.max.z - self.center.z)).abs() < epsilon {
+            } else if (center_to_point.z.abs() - (self.max().z - self.center.z)).abs() < epsilon {
                 normal.z = center_to_point.z.signum(); // Normal en el eje Z
-            } else if (center_to_point.z.abs() - (self.center.z - self.min.z)).abs() < epsilon {
+            } else if (center_to_point.z.abs() - (self.center.z - self.min().z)).abs() < epsilon {
                 normal.z = -center_to_point.z.signum(); // Normal en el eje Z (cara opuesta)
             }
-       
+
             // Obtener coordenadas UV
             let (u, v) = self.get_uv(&point, &normal);
 
@@ -108,10 +115,10 @@ impl RayIntersect for Cube {
                 distance,
                 point,
                 normal,
-                material: self.material.clone(),
-                is_intersecting: true, // Hay una intersección
-                u, // Coordenada u obtenida de get_uv
-                v, // Coordenada v obtenida de get_uv
+                material: (*self.material).clone(),  // Desreferencia Arc<Material>
+                is_intersecting: true,
+                u,
+                v,
             };
         }
 
@@ -121,10 +128,9 @@ impl RayIntersect for Cube {
             distance: 0.0,
             point: Vec3::new(0.0, 0.0, 0.0),
             normal: Vec3::new(0.0, 0.0, 0.0),
-            material: self.material.clone(),
+            material: (*self.material).clone(),  // Desreferencia Arc<Material>
             u: 0.0,
             v: 0.0,
         }
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ use camera::Camera;
 use material::{Material, Texture};
 use color::Color;
 use nalgebra_glm::{Vec3, vec3};
-use sphere::Sphere;
 use cube::Cube;
 use grid::{Grid3D, GridObject}; // Importa el grid y sus objetos
 use light::Light;
@@ -25,13 +24,46 @@ use minifb::{Key, Window, WindowOptions};
 use std::sync::Arc;
 use crate::ray_intersect::RayIntersect;
 
-static TEXTURE: OnceCell<Arc<Texture>> = OnceCell::new();
+// Crear el método para cargar texturas
+// Crear el método para cargar texturas
+fn load_texture(path: &str) -> Arc<Texture> {
+    Texture::load_from_file(path).into()
+}
 
-fn get_texture() -> Arc<Texture> {
-    TEXTURE.get_or_init(|| {
-        let texture_path = "C:/Users/irvin/UVG/Sexto_Semestre/Graficas/release/texture.png";
-        Texture::load_from_file(texture_path)
-    }).clone()
+// Define las texturas y materiales usando Arc
+fn define_materials() -> Vec<Arc<Material>> {
+    vec![
+        // Moss
+        Arc::new(Material {
+            diffuse: Color::new(40, 150, 40),
+            specular: 10.0,
+            albedo: [0.5, 0.3, 0.1, 0.0],
+            refractive_index: 1.0,
+            transparency: 0.0,
+            texture: Some(load_texture("C:/Users/irvin/UVG/Sexto_Semestre/Graficas/release/textures/moss (1).png")),
+            has_texture: true,
+        }),
+        // Dirt
+        Arc::new(Material {
+            diffuse: Color::new(150, 100, 50),
+            specular: 5.0,
+            albedo: [0.4, 0.2, 0.1, 0.0],
+            refractive_index: 1.0,
+            transparency: 0.0,
+            texture: Some(load_texture("C:/Users/irvin/UVG/Sexto_Semestre/Graficas/release/textures/dirt.png")),
+            has_texture: true,
+        }),
+        // Stone
+        Arc::new(Material {
+            diffuse: Color::new(128, 128, 128),
+            specular: 8.0,
+            albedo: [0.4, 0.2, 0.2, 0.0],
+            refractive_index: 1.2,
+            transparency: 0.0,
+            texture: Some(load_texture("C:/Users/irvin/UVG/Sexto_Semestre/Graficas/release/textures/stone.png")),
+            has_texture: true,
+        }),
+    ]
 }
 
 fn main() {
@@ -40,63 +72,116 @@ fn main() {
 
     let mut framebuffer = Framebuffer::new(width, height);
 
-    let texture = get_texture();
-
-    let sphere_material = Material {
-        diffuse: Color::new(255, 182, 193), // Color rosa pastel
-        specular: 50.0,
-        albedo: [0.6, 0.3, 0.1, 0.0],
-        refractive_index: 1.5,
-        transparency: 0.0,
-        texture: None,  
-        has_texture: false,
-    };
-
-    let cube_material = Material {
-        diffuse: Color::new(255, 255, 255),
-        specular: 50.0,
-        albedo: [0.6, 0.3, 0.1, 0.0],
-        refractive_index: 1.5,
-        transparency: 0.0,
-        texture: Some(texture.clone()),
-        has_texture: true,
-    };
+    // Definir los materiales
+    let materials = define_materials();
+    let moss_material = materials[0].clone();
+    let dirt_material = materials[1].clone();
+    let stone_material = materials[2].clone();
 
     // Crear un grid de 10x10x10
     let mut grid = Grid3D::new(10);
 
-    // Posicionar objetos en el grid
-    grid.place_object(1, 1, 1, GridObject::Cube);   // Posicionar un cubo en (1, 1, 1)
-    grid.place_object(5, 5, 5, GridObject::Sphere); // Posicionar una esfera en (5, 5, 5)
+    // Tamaño reducido de los cubos
+    let cube_size = 0.5;
 
     let mut objects: Vec<Box<dyn RayIntersect>> = Vec::new();
 
-    // Crear objetos en el grid según lo que hemos colocado
-    for x in 0..10 {
-        for y in 0..10 {
-            for z in 0..10 {
-                match grid.get_object(x, y, z) {
-                    GridObject::Cube => {
-                        let cube = Cube::new(
-                            Vec3::new(x as f32, y as f32, -(z as f32 + 5.0)), // Posicionar el cubo en la escena
-                            1.0,
-                            cube_material.clone(),
-                        );
-                        objects.push(Box::new(cube));
-                    }
-                    GridObject::Sphere => {
-                        let sphere = Sphere::new(
-                            Vec3::new(x as f32, y as f32, -(z as f32 + 5.0)), // Posicionar la esfera en la escena
-                            0.5,
-                            sphere_material.clone(),
-                        );
-                        objects.push(Box::new(sphere));
-                    }
-                    _ => {}
-                }
+    // Borde superior e inferior del rectángulo de moss (sin espacio entre los cubos)
+    for x in 0..5 {
+        // Parte superior (fila 1)
+        let cube_top = Cube::new(
+            Vec3::new(x as f32 * cube_size, 0.0, 0.0), // Posicionar los cubos pegados en el nivel z = 0
+            cube_size,                                  // Tamaño del cubo
+            moss_material.clone(),  // Usar Arc<Material> para optimizar memoria
+        );
+        objects.push(Box::new(cube_top));
+
+        // Parte inferior (fila 6)
+        let cube_bottom = Cube::new(
+            Vec3::new(x as f32 * cube_size, 0.0, -cube_size * 5.0), // Posicionar los cubos pegados en la fila inferior
+            cube_size,                                               // Tamaño del cubo
+            moss_material.clone(),  // Usar Arc<Material> para optimizar memoria
+        );
+        objects.push(Box::new(cube_bottom));
+    }
+
+    // Lados del rectángulo de moss (sin espacio entre los cubos)
+    for z in 1..5 {
+        // Lado izquierdo
+        let cube_left = Cube::new(
+            Vec3::new(0.0, 0.0, -(z as f32 * cube_size)), // Posicionar los cubos pegados en el lado izquierdo
+            cube_size,                                    // Tamaño del cubo
+            moss_material.clone(),  // Usar Arc<Material> para optimizar memoria
+        );
+        objects.push(Box::new(cube_left));
+
+        // Lado derecho
+        let cube_right = Cube::new(
+            Vec3::new(4.0 * cube_size, 0.0, -(z as f32 * cube_size)), // Posicionar los cubos pegados en el lado derecho
+            cube_size,                                                 // Tamaño del cubo
+            moss_material.clone(),  // Usar Arc<Material> para optimizar memoria
+        );
+        objects.push(Box::new(cube_right));
+    }
+
+    // Añadir los 30 cubos de dirt (dos paredes de 15 cubos cada una)
+    for z in 4..=5 { // Z = 4 para la primera pared, Z = 5 para la segunda
+        for x in 0..5 {
+            // Bloques de dirt en z-index 2, 3, y 4 encima del borde del fondo
+            for y in 1..=3 {
+                let cube_dirt = Cube::new(
+                    Vec3::new(x as f32 * cube_size, (y as f32) * cube_size, -(z as f32 * cube_size)), // Posición en el grid
+                    cube_size,                                                                       // Tamaño del cubo
+                    dirt_material.clone(),  // Usar Arc<Material> para optimizar memoria
+                );
+                objects.push(Box::new(cube_dirt));
             }
         }
     }
+
+    
+    // **Corregido**: Añadir solo una fila de 3 bloques de altura en la parte del fondo a la izquierda con el material stone
+    for y in 1..=3 { // 3 bloques de altura
+        let cube_stone = Cube::new(
+            Vec3::new(-cube_size, (y as f32) * cube_size, -5.0 * cube_size), // Columna izquierda, en el eje X = -cube_size
+            cube_size,  // Tamaño del cubo
+            stone_material.clone(),  // Usar Arc<Material> para optimizar memoria
+        );
+        objects.push(Box::new(cube_stone));
+    }
+
+    // Añadir un cubo individual de stone en la posición correcta
+    // Posición en la misma coordenada Z de la columna eliminada, a la misma altura que los otros dirts
+    let single_stone_cube = Cube::new(
+        Vec3::new(-cube_size, 3.0 * cube_size, -4.0 * cube_size),  // Ajusta la posición aquí
+        cube_size,  // Tamaño del cubo
+        stone_material.clone(),  // Usar el material de stone
+    );
+
+    // Añadir el cubo a la lista de objetos
+    objects.push(Box::new(single_stone_cube));
+
+
+        // Añadir dos cubos de dirt en las posiciones especificadas
+        let dirt_cube_1 = Cube::new(
+            Vec3::new(-cube_size, 2.0 * cube_size, -4.0 * cube_size),  // Posición para el primer cubo de dirt
+            cube_size,  // Tamaño del cubo
+            dirt_material.clone(),  // Usar el material de dirt
+        );
+    
+        let dirt_cube_2 = Cube::new(
+            Vec3::new(-cube_size, 1.0 * cube_size, -4.0 * cube_size),  // Posición para el segundo cubo de dirt
+            cube_size,  // Tamaño del cubo
+            dirt_material.clone(),  // Usar el material de dirt
+        );
+    
+        // Añadir los cubos a la lista de objetos
+        objects.push(Box::new(dirt_cube_1));
+        objects.push(Box::new(dirt_cube_2));
+    
+
+
+
 
     // Luz ambiental e iluminación de la escena
     let light = Light::new(
@@ -112,7 +197,7 @@ fn main() {
     );
 
     let mut window = Window::new(
-        "Raytracer with Textured Cube and Pastel Pink Sphere",
+        "Raytracer with Moss, Dirt and Stone",
         width,
         height,
         WindowOptions::default(),
